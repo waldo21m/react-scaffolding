3cb4524b07c8c20aaa74700d2e0538bd8bfbd877
Eduardo Márquez <eduardo20.3263@gmail.com>
2023-12-11 12:08:27 -0400

    Add debug config and changes in eslint

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,17 +1,17 @@
-import App from './App'
-import { fireEvent, render, screen } from './utils/test-utils'
+import { fireEvent, render, screen } from './utils/test-utils';
+import App from './App';
 
 describe('Simple working test', () => {
-  it('The title Vite + React is visible', () => {
-    render(<App />)
-    expect(screen.getByText(/Vite \+ React/i)).toBeInTheDocument()
-  })
+	it('The title Vite + React is visible', () => {
+		render(<App />);
+		expect(screen.getByText(/vite \+ react/i)).toBeInTheDocument();
+	});
 
-  it('Should increment count on click', async () => {
-    const { getByText } = render(<App />)
-    const button = getByText(/count is/i)
-    expect(button).toBeInTheDocument()
-    fireEvent.click(button)
-    expect(getByText(/count is 1/i)).toBeInTheDocument()
-  })
-})
+	it('Should increment count on click', () => {
+		render(<App />);
+		const button = screen.getByText(/count is/i);
+		expect(button).toBeInTheDocument();
+		fireEvent.click(button);
+		expect(screen.getByText(/count is 1/i)).toBeInTheDocument();
+	});
+});
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,15 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
-  },
-})
+	plugins: [react()],
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: './src/test/setup.ts',
+	},
+});
